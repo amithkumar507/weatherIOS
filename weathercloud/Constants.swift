0cfb68ba37a2963f8fc07fd494fe8e118b89eaef
amithkumar507 <amithkumar507@gmail.com>
2018-05-08 18:46:13 -0400

    Update Constants.swift

--- weathercloud/Constants.swift
+++ weathercloud/Constants.swift
@@ -18,5 +18,5 @@ let API_KEY = "dc6e0d2c59f067202928c8038a3d5094"
 typealias DownloadComplete = ( ) -> ( )
 
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)19\(LONGITUDE)13\(API_ID)\(API_KEY)"
-let FORECAST_URL = "https://samples.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=b1b15e88fa797225412429c1c50c122a1"
+let FORECAST_URL = "https://samples.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=""
 
